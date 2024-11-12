cb2a06bdb9de4051107418dae5ffbc0add110e13
Auguste.fleury <auguste.fleury@etu.uca.fr>
2024-11-12 15:47:37 +0100

    🚧 ajout tp3

---
+++ tp3/src/vehicule.java
@@ -0,0 +1,34 @@
+public class vehicule {
+
+
+    private String sorte;
+
+    public String getSorte() {
+        return sorte;
+    }
+
+    protected void setSorte(String sorte) {
+        this.sorte = sorte;
+    }
+
+    private String marque;
+
+    public String getMarque() {
+        return marque;
+    }
+
+    protected void setMarque(String marque) {
+        this.marque = marque;
+    }
+
+    private String couleur;
+
+    public String getCouleur() {
+        return couleur;
+    }
+
+    protected void setCouleur(String couleur) {
+        this.couleur = couleur;
+    }
+
+}
